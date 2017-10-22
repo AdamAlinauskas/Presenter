61ec0eb59638800fcd0893c6ac3a35d404da5967
Andy <andy@andypaxo.net>
2017-10-22 09:00:51 -0600

    Make migrator more robust against hitting the database while it is still starting up.

--- Migrator/Program.cs
+++ Migrator/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using Dapper;
 
 namespace Migrator
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             Console.WriteLine("Running migrations...");
+
+            if (!WaitForDatabaseToComeOnline())
+                return 42;
 
             IList<string> orgs;
             using (var sharedMigrator = GetSharedMigrator())
@@ -21,6 +26,31 @@ namespace Migrator
             using (var orgMigrator = GetOrgMigrator(org)) {
                 orgMigrator.Migrate();                
             }
+
+            return 0;
+        }
+
+        private static bool WaitForDatabaseToComeOnline()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                try
+                {
+                    var connector = new Connector();
+                    using (var connection = connector.Connect("public"))
+                        connection.Execute("SELECT 1 FROM pg_catalog.pg_database");
+                    Console.WriteLine("Connected to database");
+                    return true;
+                }
+                catch (Npgsql.PostgresException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine($"Connection attempt {i+1} failed. Retrying...");
+                    Thread.Sleep(1000);
+                }
+            }
+
+            return false;
         }
 
         private static SharedSchemaMigrator GetSharedMigrator()
