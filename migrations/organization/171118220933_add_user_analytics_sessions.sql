8d0f44cfa83c54e0c766754366adb81f1cf49ad3
Adam Alinauskas <aalinauskas@gmail.com>
2017-11-18 15:56:39 -0700

    migration script for user analytics session and page analytics

---
+++ migrations/organization/171118220933_add_user_analytics_sessions.sql
@@ -0,0 +1,26 @@
+START TRANSACTION;
+
+CREATE TABLE user_analytics_sessions (
+    id bigserial PRIMARY KEY,
+    presentation_id bigint REFERENCES presentations(id),
+    file_id bigint REFERENCES files(id),
+    user_id bigint NOT NULL,
+    created_at timestamp NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
+    created_by bigint NOT NULL,
+    longitude numeric,
+    latitude numeric,
+    country text,
+    ip_address text,
+    CONSTRAINT chk_user_analytics_sessions CHECK ( (presentation_id IS NOT NULL AND file_id IS NULL) 
+                                            or 
+                                            (file_id IS NOT NULL AND presentation_id IS NULL) )
+);
+
+CREATE TABLE page_analytics (
+    id bigserial PRIMARY KEY,
+    user_analytics_sessions_id bigint REFERENCES user_analytics_sessions(id),
+    page_number numeric NOT NULL,
+    time_on_page numeric NOT NULL DEFAULT(0)
+);
+
+COMMIT;
